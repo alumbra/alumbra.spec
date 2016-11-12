cb95ea88919f213a7c8ef288bffd532ff74704d6
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-12 15:27:21 +0100

    add fspecs for alumbra components.

--- src/alumbra/spec.clj
+++ src/alumbra/spec.clj
@@ -5,4 +5,56 @@
              document
              parser-errors
              schema
-             validation-errors]))
+             validation-errors]
+            [clojure.spec :as s]))
+
+;; ## Values
+
+(let [stream? #(instance? java.io.InputStream %)]
+  (def ^:private parser-input?
+    (s/alt :string string?
+           :stream stream?)))
+
+(s/def ::parser-errors
+  (s/keys :req [:alumbra/parser-errors]))
+
+(s/def ::validation-errors
+  (s/keys :req [:alumbra/validation-errors]))
+
+;; ## Components
+
+(s/def ::document-parser
+  (s/fspec
+    :args (s/cat :document parser-input?)
+    :ret  (s/alt :success :alumbra/document
+                 :failure ::parser-errors)))
+
+(s/def ::schema-parser
+  (s/fspec
+    :args (s/cat :document parser-input?)
+    :ret  (s/alt :success :alumbra/schema
+                 :failure ::parser-errors)))
+
+(s/def ::analyzer
+  (s/fspec
+    :args (s/cat :schema :alumbra/schema)
+    :ret  :alumbra/analyzed-schema))
+
+(s/def ::validator
+  (s/fspec
+    :args (s/cat :document  :alumbra/document
+                 :variables (s/? map?))
+    :ret  (s/alt :success nil?
+                 :failure ::validation-errors)))
+
+(s/def ::canonicalizer
+  (s/fspec
+    :args (s/cat :document       :alumbra/document
+                 :operation-name string?
+                 :variables      map?)
+    :ret  :alumbra/canonical-operation))
+
+(s/def ::executor
+  (s/fspec
+    :args (s/cat :operation :alumbra/canonical-operation)
+    :ret  map?))
