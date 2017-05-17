27a0a8c6a09c403ecee9c5490a8ed64edd43c888
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-05-17 17:02:44 +0200

    Version 0.1.11-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alumbra/spec "0.1.10"
+(defproject alumbra/spec "0.1.11-SNAPSHOT"
   :description "GraphQL Data Structure Specs for Clojure"
   :url "https://github.com/alumbra/alumbra.spec"
   :license {:name "MIT License"
